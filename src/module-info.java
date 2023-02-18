37bf11483f06766345b513a2f8fef4f66111416e
victorlitaiff <110944280+victorlitaiff@users.noreply.github.com>
2023-02-17 20:26:01 -0400

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Victor Litaiff
+ *
+ */
+module variaveis {
+}
---
+++ src/variaveis/Tipos_de_variaveis.java
@@ -0,0 +1,26 @@
+package variaveis;
+
+public class Tipos_de_variaveis {
+
+	public static void main(String[] args) {
+		int idade; 
+		int livros;
+		int cadernos;
+		int canetas;
+		
+		idade = 18;
+		livros = 30;
+		cadernos = 2;
+		canetas = 4;
+		
+			System.out.println(livros + cadernos);
+	
+		Double salario_do_funcionario = 1300.50;
+		Double bonus_mensal = 250;
+		Double horas_trabalhadas = 2.0;
+	
+		System.out.println('O salario do funcionario é'  );
+ 	}
+	
+
+}
